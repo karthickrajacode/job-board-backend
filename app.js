36a964c498f61e6b8ee2c23a920cfce98845437a
karthick <kart87632@gmail.com>
2024-06-08 12:20:04 +0530

    app.js updated by import cors & app.use cors in origin added FRONTEND_URL method (get. post, put, delete),  import cookieParser & app.use( cookieParser()). config folder upadted config.env file added FRONTEND_URL

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config({ path: "./config/config.env" });
+
+app.use(
+    cors({
+        origin: [process.env.FRONTEND_URL],
+        methods: ["GET", "POST", "DELETE", "PUT"],
+        credentials: true,
+    })
+);
+
+app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 export default app;
