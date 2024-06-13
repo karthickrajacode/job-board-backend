b0cbc496358e42b514bd6f2bc0cf51f876389b40
karthick <kart87632@gmail.com>
2024-06-13 19:54:45 +0530

    Updating the userRouter.js import the login and added login .

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,11 @@
 import express from "express";
-import {register} from "../controllers/userController.js"
+import { register } from "../controllers/userController.js"
+import { login } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", register)
+router.post("/login", login)
+
 
 export default router;
