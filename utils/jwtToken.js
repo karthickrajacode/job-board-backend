269cc56d7195235b2dadc5e7c895f8f9d742b755
karthick <kart87632@gmail.com>
2024-06-13 19:53:13 +0530

    updating the jwtToken .js  change the getJWTToken().

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,5 +1,5 @@
 export const sendToken = (user, statusCode, res, message) => {
-    const token = user.geJWTToken();
+    const token = user.getJWTToken();
     const options = {
         expires: new Date(
             Date.now() + process.env.COOKIE - EXPIRE * 24 * 60 * 60 * 1000
