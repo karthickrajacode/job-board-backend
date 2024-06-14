3613d8f1c0c864e0631b8c7d629a17d6e2aded98
karthick <kart87632@gmail.com>
2024-06-14 16:45:16 +0530

    Created the JobRouter.js in routes and Import the getAllJobs and use.

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { getAllJobs } from "../controllers/jobControllers.js"
 
 const router = express();
+
+router.get("/getall", getAllJobs)
+
 
 export default router;
