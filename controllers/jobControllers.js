e24c62794a97c505a2095cf16a3f0475d4e22a09
karthick <kart87632@gmail.com>
2024-06-14 16:40:41 +0530

    Debugging the export in jobController.js file.

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -2,7 +2,7 @@ import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { Job } from "../models/jobSchema.js";
 
 
-const getAllJobs = catchAsyncError(async (req, res, next) => {
+export const getAllJobs = catchAsyncError(async (req, res, next) => {
     const jobs = await Job.find({ expired: false })
     res.status(200).json({
         success: true,
