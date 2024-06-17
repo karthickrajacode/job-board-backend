263c84756ee4ddc28289cc96b74a69dfdb742ebd
karthick <kart87632@gmail.com>
2024-06-17 13:06:17 +0530

    Updated by applicationController.js file. Export jobSeekerDeleteApplication.

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -19,3 +19,23 @@ export const employerGetAllApplication = catchAsyncError(async (req, res, next) 
     })
 });
 
+export const jobSeekerDeleteApplication = catchAsyncError(async (req, res, next) => {
+    const { role } = req.user;
+    if (role === "job seeker") {
+        return next(new ErrorHandler(
+            "Job seeker is not allowed access is resources!",
+            400
+        )
+        );
+    }
+    const { id } = req.params;
+    const application = await Application.findById(id);
+    if (!application) {
+        return next(new ErrorHandler("Oops,application not found!", 404));
+    }
+    await application.deleteOne()
+    res.status(200).json({
+        success: true,
+        message: "Application Delete Succesfully! "
+    })
+});
