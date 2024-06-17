e0e32aaf0f4aa6fa11c15c86a0ee90a84bcfc94d
karthick <kart87632@gmail.com>
2024-06-17 11:36:13 +0530

    Updated by jobRouter.js file. Import the updateJob and deleteJob and use.

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllJobs, getmyJobs, postJob } from "../controllers/jobControllers.js"
+import { deleteJob, getAllJobs, getmyJobs, postJob, updateJob } from "../controllers/jobControllers.js"
 import { isAuthorized } from "../middlewares/auth.js"
 
 const router = express.Router();
@@ -7,6 +7,8 @@ const router = express.Router();
 router.get("/getall", getAllJobs)
 router.post("/post", isAuthorized, postJob)
 router.get("/getmyjobs", isAuthorized, getmyJobs)
+router.put("/update/:id", isAuthorized, updateJob)
+router.put("/delete/:id", isAuthorized, deleteJob)
 
 
 
