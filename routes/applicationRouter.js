93a1b4b18787ad12a8ea32f37a52dacede7bc559
karthick <kart87632@gmail.com>
2024-06-17 13:46:57 +0530

    Updated by applicationRouter.js file. Import jobSeekerDeleteApplications and use Delete method.

--- routes/applicationRouter.js
+++ routes/applicationRouter.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { employerGetAllApplications, jobSeekerGetAllApplications } from "../controllers/applicationController.js";
+import {
+    employerGetAllApplications,
+    jobSeekerDeleteApplications,
+    jobSeekerGetAllApplications
+} from "../controllers/applicationController.js";
 import { isAuthorized } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.get("/jobseeker/getall", isAuthorized, employerGetAllApplications);
-router.get("/employer/getall",isAuthorized,jobSeekerGetAllApplications)
+router.get("/employer/getall", isAuthorized, jobSeekerGetAllApplications);
+router.delete("/delete/:id", isAuthorized, jobSeekerDeleteApplications);
 
 export default router;
