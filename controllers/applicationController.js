bdf1cb1927a37bfe8ef9374b07d4ee19845733b9
karthick <kart87632@gmail.com>
2024-06-19 11:04:47 +0530

    Updated by  applicationController.js file. Import cloudinary and job and used.

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,9 +1,11 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../middlewares/error.js";
 import { Application } from "../models/applicationSchema.js";
+import cloudinary from "cloudinary";
+import { Job } from "../models/jobSchema.js";
 
 export const employerGetAllApplications = catchAsyncError(async (req, res, next) => {
-    const { role } = req.user;
+    const { role } = req.user; 
     if (role === "Job Seeker") {
         return next(new ErrorHandler(
             "Job Seeker is not allowed access is resources!",
@@ -73,4 +75,60 @@ export const postApplication = catchAsyncError(async (req, res, next) => {
     if (!allowedFormats.includes(resume.mimetypes)) {
         return next(new ErrorHandler("Invalid the types. Please upload you resume PNG or JPG and WEBP formats."))
     }
+    const cloudinary = await cloudinary.uploader.upload(
+        resume.tempFilePath
+    );
+    if (!cloudinaryResponse || cloudinaryResponse.error) {
+        console.error(
+            "cloudinary Error:",
+            cloudinaryResponse.error || "Unknown cloudinary error"
+        );
+        return next(new ErrorHandler("Faile to upload resume.", 500));
+    }
+    const { name, email, coverletter, phone, address, jobid } = req.body;
+    const applicantID = {
+        user: req.user._id,
+        role: "job seeker"
+    }
+    if (!jobId) {
+        return next(new ErrorHandler("Job not found!", 404));
+    }
+    const jobDetails = await Job.findById(jobId);
+    if (!jobDetails) {
+        return next(new ErrorHandler("Job not found!", 404));
+    }
+    const employerID = {
+        user: jobDetails.postedBy,
+        role: "Employer"
+    }
+    if (
+        !name ||
+        !email ||
+        !coverletter ||
+        !phone ||
+        !address ||
+        !applicantID ||
+        !employerID ||
+        !resume
+    ) {
+        return next(new ErrorHandler("Please fil all field!", 400))
+    }
+    const application = await Application.create({
+        name,
+        email,
+        coverletter,
+        phone,
+        address,
+        applicantID,
+        employerID,
+        resume: {
+            public_id: clouddinaryResponse.public_id,
+            url: cloudinaryResponse.secure_url,
+        },
+    });
+    res.status(200).json({
+        success: true,
+        message: "Application Submitted!",
+        application,
+    });
 });
