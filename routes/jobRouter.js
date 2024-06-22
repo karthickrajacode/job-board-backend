522fe1c4d9055bfe46b7b6253ff887097c501e4c
karthick <kart87632@gmail.com>
2024-06-22 15:35:39 +0530

    Updated by jobRouter.js file. Api added for getsingleJob.

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,15 +1,14 @@
 import express from "express";
-import { deleteJob, getAllJobs, getmyJobs, postJob, updateJob } from "../controllers/jobControllers.js"
+import { deleteJob, getAllJobs, getSingleJob, getmyJobs, postJob, updateJob } from "../controllers/jobControllers.js"
 import { isAuthorized } from "../middlewares/auth.js"
 
 const router = express.Router();
 
-router.get("/getall", getAllJobs)
-router.post("/post", isAuthorized, postJob)
-router.get("/getmyjobs", isAuthorized, getmyJobs)
-router.put("/update/:id", isAuthorized, updateJob)
-router.put("/delete/:id", isAuthorized, deleteJob)
-
-
+router.get("/getall", getAllJobs);
+router.post("/post", isAuthorized, postJob);
+router.get("/getmyjobs", isAuthorized, getmyJobs);
+router.put("/update/:id", isAuthorized, updateJob);
+router.put("/delete/:id", isAuthorized, deleteJob);
+router.get("/:id", isAuthorized, getSingleJob);
 
 export default router;
